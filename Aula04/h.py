9efc6040926cb4649e6b3381df28c140d2e3dfa0
ead <camyloti@gmail.com>
2026-04-30 08:41:11 -0300

    os arquivos feitos na atividade 4

---
+++ Aula04/h.py
@@ -0,0 +1,10 @@
+'''
+
+    Eu realmente não sei como eu vou poder fazer isso em uma hora mas TABOM ENTÃO NE-
+
+'''
+
+print (30*"=", "Atividade", 30*"=")
+print ("\n Digite um numero de 1 - 20")
+
+
