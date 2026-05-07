9225e659b29a74fbedcd6b320674f9bda09b8f74
MiguelFBAS <miguelfonsecapessoal@gmail.com>
2026-05-07 11:41:40 -0300

    aaaaaa

--- Documents/MiguelFBAS - Aulas06 em diate/Aula07/app.py
+++ Documents/MiguelFBAS - Aulas06 em diate/Aula07/app.py
@@ -1,3 +1,17 @@
 '''
-    Manipulação de arquivos:
+Manipulação de arquivos: percorrer os meus diretorios, encontrar o arquivo passar o comando de abertura de arquivos, passar comando de ação
+
+Modos de ação: 
+-  "r": Leitura do arquivo
+-  "w": escrita(sobrescrever o conteudo antigo)
+-  "a": adiciona conteudo
+-  "x": criar um arquivo 
+-  "b": arquivos binarios
+-  "t": texto
+
 '''
+#Criando e escrevendo arquivo
+
+arquivo = open("primeiro_arquivo.txt", 'w')
+arquivo.write('Hello monkeys')
+arquivo.close()
