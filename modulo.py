f353af34d62d0e826d4bc2a4702896bbcac81ee5
MiguelFBAS <miguelfonsecapessoal.gmail.com>
2026-05-12 10:36:08 -0300

    Adicionar Aula8

---
+++ modulo.py
@@ -0,0 +1,17 @@
+import os
+def soma(a, b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def multiplicacao(a,b):
+    return a * b
+
+def divisao(a,b):
+        return a / b
+if any == 0:
+         "Não é possível dividir por zero."
+
+def limpar_terminal():
+    os.system("cls" if os.name == "nt" else "clear")
---
+++ run.py
@@ -0,0 +1,4 @@
+from Programa01 import main
+
+if __name__ == "__main__":
+   main()
